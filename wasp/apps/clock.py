f518673ee6178d932103674eecefaa98618fe33f
Andreas Lefevre <andreas.lefevre@gmx.de>
2021-01-03 16:49:45 +0100

    change clock to display calender for test - need to be checked on HW

--- wasp/apps/clock.py
+++ wasp/apps/clock.py
@@ -9,16 +9,18 @@ Shows a time (as HH:MM) together with a battery meter and the date.
 
 import wasp
 
+#from wasp import watch.drawable as draw
+
 import icons
 import fonts.clock as digits
+import gregorian_calender as greg_cal
+
 
 DIGITS = (
         digits.clock_0, digits.clock_1, digits.clock_2, digits.clock_3,
         digits.clock_4, digits.clock_5, digits.clock_6, digits.clock_7,
         digits.clock_8, digits.clock_9
 )
-
-MONTH = 'JanFebMarAprMayJunJulAugSepOctNovDec'
 
 class ClockApp():
     """Simple digital clock application.
@@ -31,6 +33,19 @@ class ClockApp():
     NAME = 'Clock'
     ICON = icons.clock
 
+    def __init__(self):
+        self._fakeTime = False
+        self._fakeDay = greg_cal.Day(31, 12, 1848)
+
+        self._min = -1
+        self._day = -1
+        self._year = -1
+
+        # init the calender
+        now = wasp.watch.rtc.get_localtime()
+        self.year = greg_cal.Year(now[0])
+        self.calDay = greg_cal.Day(now[2], now[1], now[0])
+
     def foreground(self):
         """Activate the application.
 
@@ -40,6 +55,13 @@ class ClockApp():
         wasp.system.bar.clock = False
         self._draw(True)
         wasp.system.request_tick(1000)
+        wasp.system.request_event(wasp.EventMask.TOUCH)
+
+        draw = wasp.watch.drawable
+        hi =  wasp.system.theme('accent-hi')
+        mid = wasp.system.theme('accent-mid')
+        lo =  wasp.system.theme('accent-lo')
+
 
     def sleep(self):
         """Prepare to enter the low power mode.
@@ -62,6 +84,19 @@ class ClockApp():
         """Periodic callback to update the display."""
         self._draw()
 
+    def _clear(self):
+        draw = wasp.watch.drawable
+        hi =  wasp.system.theme('accent-hi')
+        mid = wasp.system.theme('accent-mid')
+        lo =  wasp.system.theme('accent-lo')
+
+        # Clear the display and draw that static parts of the watch face
+        draw.fill()
+        draw.blit(digits.clock_colon, 2*48, 40, fg=mid)
+
+        # Redraw the status bar
+        wasp.system.bar.draw()
+
     def _draw(self, redraw=False):
         """Draw or lazily update the display.
 
@@ -69,42 +104,99 @@ class ClockApp():
         if the time on display has not changed. However if redraw is set to
         True then a full redraw is be performed.
         """
-        draw = wasp.watch.drawable
-        hi =  wasp.system.theme('accent-hi')
-        mid = wasp.system.theme('accent-mid')
-        lo =  wasp.system.theme('accent-lo')
 
-        if redraw:
-            now = wasp.watch.rtc.get_localtime()
-
-            # Clear the display and draw that static parts of the watch face
-            draw.fill()
-            draw.blit(digits.clock_colon, 2*48, 80, fg=mid)
-
-            # Redraw the status bar
-            wasp.system.bar.draw()
+        if self._fakeTime:
+            now = self.getFakeTime()
         else:
             # The update is doubly lazy... we update the status bar and if
             # the status bus update reports a change in the time of day 
             # then we compare the minute on display to make sure we 
             # only update the main clock once per minute.
             now = wasp.system.bar.update()
-            if not now or self._min == now[4]:
-                # Skip the update
-                return
+        
+        if redraw or (self._day != now[2]):
+            # Record the day that is currently being displayed
+            self._clear()
+            self._drawPerDay(now)
+            redraw = True
 
-        # Format the month as text
-        month = now[1] - 1
-        month = MONTH[month*3:(month+1)*3]
+        if redraw or (self._min != now[4]):
+            # Record the minute that is currently being displayed
+            self._min = now[4]
+            self._drawPerMin(now)
 
+                
+ 
+    def _drawPerDay(self, now):
+
+        draw = wasp.watch.drawable
+        hi =  wasp.system.theme('accent-hi')
+        mid = wasp.system.theme('accent-mid')
+        lo =  wasp.system.theme('accent-lo')
+
+        # update calender
+        if self._day == now[2]:
+            pass
+        elif self._day + 1 == now[2]:
+            self.calDay.increment(1)
+        else:
+            # complied reinit - should not happen
+            self.calDay = greg_cal.Day(now[2], now[1], now[0])
+        self._day = now[2]
+
+        if self._year != now[0]:
+            self.year = greg_cal.Year(now[0])
+            self._year = now[0]
+
+        # draw current date
+        d = self.calDay
+        draw.set_color(mid)
+        draw.string('{} {} {}.{}.{}'.format(d.cw, d.weekdayShort, d.day, d.mon, d.year),
+                0, 110, width=240)
+        
+        # draw date info
+        info_line = 0
+        tmp = self.year.isSpecialDay(d.day, d.mon)
+        if len(tmp) == 0:
+            draw.set_color(lo)
+            draw.string('no special day', 0, 140+24*info_line, width=240)
+            info_line += 1
+        else:
+            for item in tmp:
+                s = item.name
+                if item.type == greg_cal.SpecialDayType.IGNORE:
+                    continue
+                if item.type == greg_cal.SpecialDayType.INFO_DAY:
+                    draw.set_color(lo)
+                if item.type == greg_cal.SpecialDayType.HOLIDAY:
+                    draw.set_color(hi)
+                chunks = draw.wrap(s, 240)
+                for i in range(len(chunks)-1):
+                    sub = s[chunks[i]:chunks[i+1]].rstrip()
+                    draw.string(sub, 0, 140+24*info_line, width=240)
+                    info_line += 1
+
+    def _drawPerMin(self, now):
+ 
+        draw = wasp.watch.drawable
+        hi =  wasp.system.theme('accent-hi')
+        mid = wasp.system.theme('accent-mid')
+        lo =  wasp.system.theme('accent-lo')
+ 
         # Draw the changeable parts of the watch face
-        draw.blit(DIGITS[now[4]  % 10], 4*48, 80, fg=hi)
-        draw.blit(DIGITS[now[4] // 10], 3*48, 80, fg=lo)
-        draw.blit(DIGITS[now[3]  % 10], 1*48, 80, fg=hi)
-        draw.blit(DIGITS[now[3] // 10], 0*48, 80, fg=lo)
-        draw.set_color(hi)
-        draw.string('{} {} {}'.format(now[2], month, now[0]),
-                0, 180, width=240)
+        draw.blit(DIGITS[now[4]  % 10], 4*48, 40, fg=hi)
+        draw.blit(DIGITS[now[4] // 10], 3*48, 40, fg=lo)
+        draw.blit(DIGITS[now[3]  % 10], 1*48, 40, fg=hi)
+        draw.blit(DIGITS[now[3] // 10], 0*48, 40, fg=lo)
 
-        # Record the minute that is currently being displayed
-        self._min = now[4]
+    def touch(self, event):
+        self._fakeTime = True
+    
+    def getFakeTime(self):
+        fake_now = [ 1848, 12, 31, 23, 59 ]
+        self._fakeDay.increment(1)
+        fake_now[0] = self._fakeDay.year
+        fake_now[1] = self._fakeDay.mon
+        fake_now[2] = self._fakeDay.day
+        return fake_now
+      
